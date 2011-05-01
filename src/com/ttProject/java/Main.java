1d864b8e4492b8db814615fab243508c05b4d1f0
Toda Takahiko <poepoemix@hotmail.com>
2011-05-01 09:20:51 +0900

    java sample starting entry.

--- src/com/ttProject/java/Main.java
+++ src/com/ttProject/java/Main.java
@@ -2,10 +2,14 @@ package com.ttProject.java;
 
 import com.ttProject.server.net.xmlSocket.XmlSocketMinaTransport;
 
+/**
+ * sample entry for Java
+ */
 public class Main {
 	public static void main(String[] args) {
 		XmlSocketMinaTransport transport = new XmlSocketMinaTransport();
 		try {
+			// start server
 			transport.start();
 		}
 		catch(Exception e) {
